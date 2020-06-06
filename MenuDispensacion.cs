da459d5a1d8fe22843e664a60c5fadee127aec10
michellemariedd <66342399+michellemariedd@users.noreply.github.com>
2020-06-06 01:44:46 -0400

    Add files via upload

---
+++ MenuDispensacion.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimerParcial
+{
+    public class MenuDispensacion
+    {
+        public static int dispensingMode = 3;
+
+        enum Opciones 
+        {
+            PAPELETA = 1,
+            PAPELETA1,
+            EFICIENTE,
+            BACK
+        }
+
+        public void ImprimirMenu() 
+        {
+            MenuPrincipal menuPrincipal = new MenuPrincipal();
+            
+
+            try 
+            {
+                Console.Clear();
+                Console.WriteLine("***Dispensacion de dinero***");
+                Console.WriteLine();
+                Console.WriteLine(" 1-200-1000\n 2-100 -500\n 3-Eficiente\n 4-Volver");
+                int Opcion = Convert.ToInt32(Console.ReadLine());
+                
+                switch (Opcion) 
+                {
+                    case (int)Opciones.PAPELETA:
+                        dispensingMode = 1;
+                        menuPrincipal.ImprimirMenu();
+                        break;
+                    case (int)Opciones.PAPELETA1:
+                        dispensingMode = 2;
+                        menuPrincipal.ImprimirMenu();
+                        break;
+                    case (int)Opciones.EFICIENTE:
+                        dispensingMode = 3;
+                        menuPrincipal.ImprimirMenu();
+                        break;
+                    case (int)Opciones.BACK:
+                        menuPrincipal.ImprimirMenu();
+                        break;
+                }
+            }
+            catch (Exception e ) 
+            {
+                Console.WriteLine("Debe seleccionar una opcion Valida");
+                Console.ReadKey();
+                menuPrincipal.ImprimirMenu();
+            }
+
+        }
+    }
+}
